47685e165d16fd3acabce78bd6151fd8ba6bf83e
Leo <levan2312@yandex.ru>
2023-03-30 23:30:17 +0400

    initial gitignore

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,4 +17,28 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    void incomesInBillions() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {8};
+        long expected = 8;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldReturnMaxValueWhenArrayContainsNegativeNumbers() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {5, 0, -8, 4, -5, -3, 8, 6, -11, -11, -12};
+        long expected = 8;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
